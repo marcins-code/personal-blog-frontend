ba3bedd56c15764576a10e3a8a6450caba3ebae4
marcin <marcinpaczkowski@icloud.com>
2021-03-22 10:38:42 +0100

    Add themes and assets

---
+++ src/themes/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*, *::before, *::after {
+    box-sizing:border-box;
+    margin:0;
+    padding:0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;   
+}
+
+html {
+    font-size:62.5%;
+}
+body{
+
+font-family: "Montserrat", sans-serif;
+font-size: 1.6rem;
+}
+`;
+
+export default GlobalStyle;
