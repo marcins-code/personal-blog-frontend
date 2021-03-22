b6a58b4d8639983f6fdcfb7f0b6bf17eceae18df
marcin <marcinpaczkowski@icloud.com>
2021-03-22 10:20:46 +0100

    Added Homepage and router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Homepage from './views/site/Homepage';
 
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ł Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Homepage} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/views/site/Homepage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Homepage = () => (
+  <h3>wefwewef</h3>
+);
+
+export default Homepage;
