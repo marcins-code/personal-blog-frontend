4b6aa89ba1398b49e8542fe512689ed66c7c5cd1
marcin <marcinpaczkowski@icloud.com>
2021-03-22 16:40:18 +0100

    Setup GlobalTemplate.js

--- src/templates/GlobalTemplate.js
+++ src/templates/GlobalTemplate.js
@@ -2,24 +2,79 @@ import React, { useState, useEffect } from 'react';
 // import React from 'react';
 import useWindowSize from 'hooks/useWindowSize';
 import PageContext from 'context';
-// import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'themes/GlobalStyle';
 import PropTypes from 'prop-types';
+import {
+  commonTheme, darkTheme, lightTheme, chocoladeTheme,
+} from 'themes/Theme';
 
 const GlobalTemplate = (props) => {
   const [isMobile, setIsMobile] = useState(false);
-
+  const [appTheme, setAppTheme] = useState('dark');
+  const [sidebarTheme, setSidebarTheme] = useState('dark');
+  const [navPosition, setNavPosition] = useState('menu-top');
+  const [lang, setLang] = useState('pl');
   const size = useWindowSize();
   useEffect(() => {
     setIsMobile(size.width <= 760);
   }, [size]);
 
+  const appThemeHandler = (e) => {
+    setAppTheme(e.target.value);
+  };
+
+  const sidebarThemeHandler = (e) => {
+    setSidebarTheme(e.target.value);
+  };
+
+  const navPositionHandler = (e) => {
+    setNavPosition(e.target.checked ? 'sidebar' : 'menu-top');
+  };
+
+  const langSwitchHandler = (e) => {
+    setLang(e.target.checked ? 'en' : 'pl');
+  };
+
+  let theme;
+  switch (sidebarTheme) {
+    case 'dark': {
+      theme = { ...commonTheme, ...darkTheme };
+      break;
+    }
+
+    case 'light': {
+      theme = { ...commonTheme, ...lightTheme };
+      break;
+    }
+
+    case 'chocolade': {
+      theme = { ...commonTheme, ...chocoladeTheme };
+      break;
+    }
+
+    default:
+      theme = { ...commonTheme, ...darkTheme };
+  }
+
   return (
     <PageContext.Provider
       value={{
         isMobile,
+        appTheme,
+        sidebarTheme,
+        navPosition,
+        lang,
+        appThemeHandler,
+        sidebarThemeHandler,
+        navPositionHandler,
+        langSwitchHandler,
       }}
     >
-      <>{props.children}</>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <>{props.children}</>
+      </ThemeProvider>
     </PageContext.Provider>
   );
 };
