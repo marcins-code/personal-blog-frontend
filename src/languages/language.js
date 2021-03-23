9e6ef18e3de81d5bc7188c966f9369e1c28865d9
marcin <marcinpaczkowski@icloud.com>
2021-03-23 08:04:07 +0100

    Add languude file

---
+++ src/languages/language.js
@@ -0,0 +1,17 @@
+export const langPl = {
+  menu: {
+    homepage: 'Homepage',
+    categories: 'Kategorie',
+    series: 'Serie',
+    contact: 'Kontakt',
+  },
+};
+
+export const langEN = {
+  menu: {
+    homepage: 'Homepage',
+    categories: 'Categories',
+    series: 'Series',
+    contact: 'Contact',
+  },
+};
