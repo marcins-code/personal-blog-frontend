93dd4bdd0741e67bdef7708fad621a17f23a5c2e
marcin <marcinpaczkowski@icloud.com>
2021-03-23 08:56:02 +0100

    Add fontawesome to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,35 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { commonTheme, darkTheme } from '../src/themes/Theme';
+import GlobalStyle from '../src/themes/GlobalStyle';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, far, fab);
+const theme = { ...darkTheme, ...commonTheme };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#2C2D2E',
+      },
+      {
+        name: 'light',
+        value: '#747474',
+      },
+    ],
+  },
+};
