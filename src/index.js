4e2d1c1fb8136b16d5ba0f1d8f811c7030d17516
marcin <marcinpaczkowski@icloud.com>
2021-03-23 08:25:47 +0100

    Install fontawesome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+library.add(fas, far, fab);
 
 ReactDOM.render(
   <React.StrictMode>
