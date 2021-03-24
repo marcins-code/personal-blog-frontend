800d6e008c5b0eb418e0da2921b835dc59f6f43f
marcin <marcinpaczkowski@icloud.com>
2021-03-24 08:41:07 +0100

    Refactor Input

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { commonTheme, darkTheme } from 'themes/Theme';
+
+const theme = { ...commonTheme, ...darkTheme };
+
+const StyledInput = styled.input`
+  height: 30px;
+  width: 100%;
+  font-size: 1.6rem;
+  padding: 0 10px;
+  background-color: ${theme.light.backgroundColor};
+  border-radius: 10px;
+  border: solid 2px ${theme.light.backgroundDarken};
+
+  &:focus,
+  :active {
+    border-color: ${theme.secondary.backgroundColor};
+    outline: none;
+  }
+`;
 
 const Input = ({
-  type, name, label, placeholder, onChange, value,
+  type, name, label, placeholder, onChange, value, onBlur, className,
 }) => (
   <>
     {label && <label htmlFor={name}>{label}</label>}
-    <input
+    <StyledInput
       type={type}
       name={name}
       id={name}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      onBlur={onBlur}
+      className={className}
     />
   </>
 );
@@ -22,16 +44,19 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string,
   placeholder: PropTypes.string,
-  value: PropTypes.string,
+  value: PropTypes.string.isRequired,
   onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  className: PropTypes.string,
 };
 
 Input.defaultProps = {
   type: 'text',
   label: '',
   placeholder: '',
-  value: '',
   onChange: undefined,
+  onBlur: undefined,
+  className: '',
 };
 
 export default Input;
