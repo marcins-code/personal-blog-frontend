8f93bb875c9ad8f91e14acd1e840cb4bfe2f2dc9
marcin <marcinpaczkowski@icloud.com>
2021-03-27 16:34:37 +0100

    Install codemirror

--- src/views/page/Homepage.js
+++ src/views/page/Homepage.js
@@ -1,9 +1,15 @@
+import Button from 'components/atoms/Button/Button';
 import React from 'react';
 import PageTemplate from 'templates/PageTemplate';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Homepage = (props) => (
   <PageTemplate>
     <h2>Homepage</h2>
+    <div>
+      <FontAwesomeIcon icon={['fas', 'coffee']} />
+      <Button icon="far address-book" btnColor="secondary" btnOutline label="Moó button" />
+    </div>
   </PageTemplate>
 );
 
