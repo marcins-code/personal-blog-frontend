c4596e3653705e4b3a0bc4f415a91ccf53493e77
marcin <marcinpaczkowski@icloud.com>
2021-03-27 16:49:21 +0100

    Add wrapper for rorms inputs

---
+++ src/components/atoms/Wrappers/InputWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const InputWrapper = styled.div`
+  padding: 10px;
+  margin-bottom: 25px;
+  position: relative;
+  display: block;
+`;
+
+export default InputWrapper;
