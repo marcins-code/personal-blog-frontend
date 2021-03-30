fe106796aa33a5ccb2fc159923e406f92f676dd7
marcin <marcinpaczkowski@icloud.com>
2021-03-30 16:43:06 +0200

    To finish

--- src/views/admin/AdminArticles.js
+++ src/views/admin/AdminArticles.js
@@ -1,10 +1,30 @@
+/* eslint-disable consistent-return */
 import React from 'react';
-import AdminTemplate from '../../templates/AdminTemplate';
+// import { useHttpClient } from 'hooks/useHttpClient';
+// import ErrorModal from 'components/atoms/ErrorModal/ErrorModal';
+// import LoadingSpinner from 'components/atoms/LoadingSpinner/LoadingSpinner';
+// import { useApi } from 'hooks/useAPI';
+import MainTemplate from 'templates/MainTemplate';
+import AdminNewArticle from './AdminNewArticle';
 
-const AdminArticles = () => (
-  <AdminTemplate>
-    <h3>Articles</h3>
-  </AdminTemplate>
-);
+const AdminArticles = () => {
+  // const [loadedArtiles, setLoadedArtiles] = useState();
+  const isLoading = false;
 
+  // const {
+  //   isLoading, error, data, status,
+  // } = useApi('http://localhost:5000/api/article', 'POST', JSON.stringify(body));
+
+  // console.log(data);
+  // console.log(error);
+  // console.log(status);
+  return (
+    <MainTemplate>
+      {/* <ErrorModal error={error} onClear={clearError} /> */}
+      {isLoading && <div className="center">{/* <LoadingSpinner /> */}</div>}
+      {!isLoading && <AdminNewArticle />}
+      <pre />
+    </MainTemplate>
+  );
+};
 export default AdminArticles;
