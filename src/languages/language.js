08f9c4f9be33a3aaa77fd3d328a68c9907a70b52
marcin <marcinpaczkowski@icloud.com>
2021-03-30 16:37:47 +0200

    Add setting panel phrazes

--- src/languages/language.js
+++ src/languages/language.js
@@ -1,17 +1,34 @@
-export const langPl = {
-  menu: {
-    homepage: 'Homepage',
-    categories: 'Kategorie',
-    series: 'Serie',
-    contact: 'Kontakt',
+export const mainMenuItems = [
+  {
+    name_en: 'homepage',
+    name_pl: 'homepage',
+    path: '/',
+    exact: true,
   },
-};
+  { name_en: 'categories', name_pl: 'kategorie', path: '/categories' },
+  { name_en: 'series', name_pl: 'serie', path: '/series' },
+  { name_en: 'contact', name_pl: 'kontakt', path: '/contact' },
+];
 
-export const langEN = {
-  menu: {
-    homepage: 'Homepage',
-    categories: 'Categories',
-    series: 'Series',
-    contact: 'Contact',
+export const adminMenuItems = [
+  {
+    name_en: 'articles',
+    name_pl: 'artykuły',
+    path: '/admin',
+    exact: true,
+  },
+  { name_en: 'Article types', name_pl: 'typy artkułów', path: '/admin/categories' },
+];
+
+export const settingsPanelItems = {
+  pl: {
+    chooseTheme: 'Wybierz motyw strony',
+    chooseLayout: 'Wybierz układ menu',
+    chooseSidebarTheme: 'Wybierz motyw menu bocznego',
+  },
+  en: {
+    chooseTheme: 'Choose theme',
+    chooseLayout: 'Choose page laout',
+    chooseSidebarTheme: 'Choose sidebar theme',
   },
 };
