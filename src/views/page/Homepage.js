17177e4acedbd1cb778cb1f4251c58c2cd5cf329
marcin <marcinpaczkowski@icloud.com>
2021-04-01 09:49:35 +0200

    Remove test code

--- src/views/page/Homepage.js
+++ src/views/page/Homepage.js
@@ -1,16 +1,10 @@
 // import Button from 'components/atoms/Button/Button';
-import React, { useContext } from 'react';
+import React from 'react';
 import MainTemplate from 'templates/MainTemplate';
-import { AuthContext } from 'context';
 
-const Homepage = (props) => {
-  const loginContext = useContext(AuthContext);
-  // const { firstName } = loginContext;
-  console.log(loginContext);
-  return (
-    <MainTemplate>
-      <div>Home</div>
-    </MainTemplate>
-  );
-};
+const Homepage = (props) => (
+  <MainTemplate>
+    <div>Home</div>
+  </MainTemplate>
+);
 export default Homepage;
