1bfb52599af7297187a5df6d898f9606f4358c3f
marcin <marcinpaczkowski@icloud.com>
2021-04-01 19:30:07 +0200

    Add flex display

--- src/components/atoms/Wrappers/InputWrapper.js
+++ src/components/atoms/Wrappers/InputWrapper.js
@@ -4,7 +4,10 @@ const InputWrapper = styled.div`
   padding: 10px;
   margin-bottom: 25px;
   position: relative;
-  display: block;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default InputWrapper;
+
+// TODO improve positioning with props
