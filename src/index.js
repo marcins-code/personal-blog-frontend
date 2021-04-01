a58fa63f9372d64473616c4b03e4e8099c2e5920
marcin <marcinpaczkowski@icloud.com>
2021-04-01 09:52:25 +0200

    Remove default css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
