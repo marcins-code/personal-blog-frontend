a39020a004919f083f87d135303343ad136ad22f
marcin <marcinpaczkowski@icloud.com>
2021-04-01 07:37:49 +0200

    Add AuthContext

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
 
 export const PageContext = React.createContext();
+
+export const AuthContext = React.createContext({
+  isLoggedIn: false,
+  firstName: null,
+  lastName: null,
+  roles: null,
+  token: null,
+  login: () => {},
+  logout: () => {},
+});
