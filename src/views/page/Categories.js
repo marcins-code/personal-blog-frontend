434d806dc981dee62aa300f2937de5e766cc690e
marcin <marcinpaczkowski@icloud.com>
2021-04-01 15:03:21 +0200

    Remove dump content

--- src/views/page/Categories.js
+++ src/views/page/Categories.js
@@ -1,21 +1,9 @@
-import Button from 'components/atoms/Button/Button';
 import React from 'react';
 import PageTemplate from 'templates/MainTemplate';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Categories = (props) => (
   <PageTemplate>
     <h2>Categories</h2>
-    <div>
-      <FontAwesomeIcon icon={['fas', 'coffee']} />
-      <Button
-        type="button"
-        labelIcon={['fas', 'check']}
-        btnColor="secondary"
-        btnOutline
-        label="Moó button"
-      />
-    </div>
   </PageTemplate>
 );
 
