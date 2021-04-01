bd7f359b416fae6a9f9f911e8cb21dea6977d16b
marcin <marcinpaczkowski@icloud.com>
2021-04-01 09:49:01 +0200

    Add Spinner for loading status

---
+++ src/components/molecules/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => (
+  <div className="spinner-wrapper">
+    <div id="circle6" />
+    <h4>ładowanie</h4>
+  </div>
+);
+
+export default Spinner;
+// TODO to finish styles
