315dc9b0ccb09ef39fec6f21548668db777b9abb
marcin <marcinpaczkowski@icloud.com>
2021-04-01 09:45:06 +0200

    Add empty components

---
+++ src/views/page/Contact.js
@@ -0,0 +1,10 @@
+// import Button from 'components/atoms/Button/Button';
+import React from 'react';
+import MainTemplate from 'templates/MainTemplate';
+
+const Homepage = (props) => (
+  <MainTemplate>
+    <div>Contact</div>
+  </MainTemplate>
+);
+export default Homepage;
