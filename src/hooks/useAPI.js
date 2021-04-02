da956f7dc2cb8771a0b513bfd4480bca2834bd63
marcin <marcinpaczkowski@icloud.com>
2021-04-02 12:43:53 +0200

    Finish useApi hook

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,44 +1,50 @@
+/* eslint-disable no-underscore-dangle */
 import { useState, useEffect } from 'react';
 
-export const useApi = (url, method = 'GET', body = null, trigger) => {
+export const useApi = (url, method = 'GET', body, trigger, setTriggerFunc) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [data, setData] = useState();
+  const [error, setError] = useState('');
+  const [resultData, setResultData] = useState();
+  const [resultDataID, setResultDataID] = useState();
   const [status, setStatus] = useState();
-
+  const [statusOK, setStatusOK] = useState();
   useEffect(() => {
-    setIsLoading(true);
-    // const addHeaders = new Headers();
-    // addHeaders.append(headers)
-    const fetchData = async () => {
-      try {
-        const res = await fetch(url, {
-          method,
-          headers: { 'Content-Type': 'application/json' },
-          body,
-        });
-        const resData = await res.json();
-        console.log(resData);
-
-        if (resData) {
-          setTimeout(() => {
-            setData(resData);
-            setStatus(res.status);
-            setIsLoading(false);
-          }, 1000);
+    if (trigger) {
+      setIsLoading(true);
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url, {
+            method,
+            headers: { 'Content-Type': 'application/json' },
+            body,
+          });
+          const responseData = await response.json();
+          setStatusOK(response.ok);
+          setStatus(response.status);
+          if (response.ok) {
+            setResultData(responseData);
+            setResultDataID(responseData._id);
+          } else {
+            setError(responseData);
+            setResultDataID(responseData._id);
+          }
+        } catch (err) {
+          setError(err);
         }
-      } catch (err) {
-        setError(err);
+        setTriggerFunc(false);
         setIsLoading(false);
-      }
-    };
-    return fetchData();
+      };
+      return fetchData();
+    }
+    return null;
   }, [trigger]);
 
   return {
     isLoading,
     error,
-    data,
+    resultData,
+    resultDataID,
     status,
+    statusOK,
   };
 };
