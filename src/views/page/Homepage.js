7a8524ef425e30b993e5d0f1718a7615f3bde5d3
marcin <marcinpaczkowski@icloud.com>
2021-04-02 16:57:03 +0200

    Clean tests

--- src/views/page/Homepage.js
+++ src/views/page/Homepage.js
@@ -1,10 +1,13 @@
-// import Button from 'components/atoms/Button/Button';
 import React from 'react';
 import MainTemplate from 'templates/MainTemplate';
 
-const Homepage = (props) => (
-  <MainTemplate>
-    <div>Home</div>
-  </MainTemplate>
-);
+const Homepage = (props) => {
+  console.log('dd');
+
+  return (
+    <MainTemplate>
+      <div>Home</div>
+    </MainTemplate>
+  );
+};
 export default Homepage;
