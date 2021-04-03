2b90cbb4f3778a91aede58514305ae28cc06091f
marcin <marcinpaczkowski@icloud.com>
2021-04-03 12:32:48 +0200

    Remove button

--- src/views/page/Authorization.js
+++ src/views/page/Authorization.js
@@ -1,19 +1,13 @@
-import React, { useState } from 'react';
-import Button from 'components/atoms/Button/Button';
+import React from 'react';
+// import Button from 'components/atoms/Button/Button';
 import MainTemplate from 'templates/MainTemplate';
 import LoginForm from 'components/organism/Forms/LoginForm';
 
-const Authorization = (props) => {
-  const [isLoggin, setIsLoggin] = useState(true);
-  console.log(isLoggin);
-
-  return (
-    <MainTemplate>
-      <LoginForm />
-      <Button type="button" label="SignUp" btnClick={() => setIsLoggin(false)} />
-    </MainTemplate>
-  );
-};
+const Authorization = (props) => (
+  <MainTemplate>
+    <LoginForm />
+  </MainTemplate>
+);
 
 Authorization.propTypes = {};
 
