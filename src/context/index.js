2e5b5f1dc0d2998419109b9e06bd9619c8f95fe1
marcin <marcinpaczkowski@icloud.com>
2021-04-05 11:16:54 +0200

    Add

--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-export const PageContext = React.createContext();
+export const PageContext = React.createContext({
+  isMobile: false,
+  lang: 'pl',
+  appTheme: 'chocolate',
+  sidebarTheme: 'chocolate',
+  navPosition: 'menu - top',
+  isAdminPage: false,
+  appThemeHandler: () => {},
+  sidebarThemeHandler: () => {},
+  navPositionHandler: () => {},
+  langSwitchHandler: () => {},
+});
 
 export const AuthContext = React.createContext({
   isLoggedIn: false,
