158363c550e449bdb93601c1b79824aa5ce6d71f
marcin <marcinpaczkowski@icloud.com>
2021-04-06 12:16:27 +0200

    Add module

---
+++ src/themes/commonElements/commonColors.js
@@ -0,0 +1,26 @@
+export const commonColors = {
+  grey100: 'hsl(0, 0%, 96%)',
+  grey200: 'hsl(0, 0%, 90%)',
+  grey300: 'hsl(0, 0%, 70%)',
+  black: 'hsl(0,0%,0%)',
+  blackHalf: 'rgba(0.0.0,.5)',
+  wax: '#2b2b2b',
+  chocolate: {
+    backgroundColor: '#622c1e',
+    backgroundColorDarken: '#33140c',
+  },
+  light: {
+    backgroundColor: '#b3b3b3',
+    backgroundColorDarken: '#808080',
+  },
+  dark: {
+    backgroundColor: '#403f3f',
+    backgroundColorDarken: '#171717',
+  },
+  blue: {
+    backgroundColor: '#006ba9',
+    backgroundColorDarken: '#004a75',
+  },
+};
+
+// 5B5A5A
