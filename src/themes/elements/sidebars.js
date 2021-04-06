34250716e98f2b1904114a082ceed3a7839e1b69
marcin <marcinpaczkowski@icloud.com>
2021-04-06 10:13:25 +0200

    Add file fo sidebars theme

---
+++ src/themes/elements/sidebars.js
@@ -0,0 +1,25 @@
+import { commonColors } from 'themes/commonElements/commonColors';
+import { backgroundImages } from 'themes/commonElements/backgroundImages';
+
+export const sidebars = {
+  dark: {
+    sidebarBackgroundColor: '#313131',
+    sidebarbackgroundImage: backgroundImages.smoothWallLigth,
+    sidebarColor: commonColors.grey200,
+  },
+  light: {
+    sidebarBackgroundColor: '#c8c8c8',
+    sidebarbackgroundImage: backgroundImages.nami,
+    sidebarColor: commonColors.black,
+  },
+  chocolate: {
+    sidebarBackgroundColor: '#622617',
+    sidebarbackgroundImage: backgroundImages.debutLight,
+    sidebarColor: commonColors.grey200,
+  },
+  blue: {
+    sidebarBackgroundColor: '#3b64b0',
+    sidebarbackgroundImage: backgroundImages.blackLinen,
+    sidebarColor: commonColors.grey200,
+  },
+};
