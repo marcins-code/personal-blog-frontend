15936275b747d730b5214a66a1c0726fbc0e082e
marcin <marcinpaczkowski@icloud.com>
2021-04-06 10:14:29 +0200

    Add background images styles

---
+++ src/themes/commonElements/backgroundImages.js
@@ -0,0 +1,19 @@
+import blackOrchid from 'assets/images/backgrounds/black-orchid.png';
+import hexellence from 'assets/images/backgrounds/hexellence.png';
+import hexellenceMore from 'assets/images/backgrounds/hexellence-more.png';
+import textile from 'assets/images/backgrounds/clean-textile.png';
+import smoothWallLigth from 'assets/images/backgrounds/smooth-wall-light.png';
+import nami from 'assets/images/backgrounds/nami.png';
+import debutLight from 'assets/images/backgrounds/debut-light.png';
+import blackLinen from 'assets/images/backgrounds/black-linen.png';
+
+export const backgroundImages = {
+  textile,
+  hexellence,
+  hexellenceMore,
+  blackOrchid,
+  smoothWallLigth,
+  nami,
+  debutLight,
+  blackLinen,
+};
