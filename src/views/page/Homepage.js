7c9aaf520f5183e052234225c622e6413e275141
marcin <marcinpaczkowski@icloud.com>
2021-04-07 08:07:43 +0200

    REmove test content

--- src/views/page/Homepage.js
+++ src/views/page/Homepage.js
@@ -1,228 +1,34 @@
+// import Button from 'components/atoms/Button/Button';
 import React from 'react';
 import MainTemplate from 'templates/MainTemplate';
 
-const Homepage = (props) => {
-  console.log('dd');
+const Homepage = (props) => (
+  <MainTemplate>
+    {/* <Button btnColor="primary" label="Zwykły" />
+    <br />
+    <br />
+    <Button btnColor="secondary" label="Zwykły" />
+    <br />
+    <br />
 
-  return (
-    <MainTemplate>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h2>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h2>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <h1>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</h1>
-        {' '}
-        At nos hinc posthac,
-        sitientis piros Afros. Contra legem facit qui id facit quod lex prohibet. Quae vero auctorem
-        tractata ab fiducia dicuntur. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-        eiusmod tempor incidunt ut labore et dolore magna aliqua.
-      </div>
-    </MainTemplate>
-  );
-};
+    <Button btnColor="dark" label="Zwykły" />
+    <br />
+    <br />
+    <Button btnColor="light" label="Zwykły" />
+    <br />
+    <br />
+    <Button btnColor="primary" label="Zwykły" btnOutline />
+    <br />
+    <br />
+    <Button btnColor="secondary" label="Zwykły" btnOutline />
+    <br />
+    <br />
+    <Button btnColor="dark" label="Zwykły" btnOutline />
+    <br />
+    <br />
+    <Button btnColor="light" label="Zwykły" btnOutline />
+    <br />
+    <br /> */}
+  </MainTemplate>
+);
 export default Homepage;
