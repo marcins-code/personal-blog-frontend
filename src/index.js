e3c364cf106f2fa6c46822ed299866f7e424370b
marcin <marcinpaczkowski@icloud.com>
2021-04-07 10:29:39 +0200

    Order fotnawesome

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faCogs, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-library.add(fas, far, fab);
+library.add(faBars, faCogs, faSignInAlt, faTimesCircle);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
