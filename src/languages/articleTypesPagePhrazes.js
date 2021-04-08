a57fa440abe0b83dcaf85d780dd295358155e742
marcin <marcinpaczkowski@icloud.com>
2021-04-08 07:02:49 +0200

    Add new translation file

---
+++ src/languages/articleTypesPagePhrazes.js
@@ -0,0 +1,22 @@
+export const articleTypesPagePhrazes = {
+  en: {
+    title: 'Categories and series',
+    newType: 'Add new type',
+    titlePL: 'Title pl',
+    titleEN: 'Title en',
+    type: 'Type',
+    createdAt: 'Created at',
+    modifiedAt: 'Modified at',
+    author: 'Author',
+  },
+  pl: {
+    title: 'Kategorie i serie',
+    newType: 'Dodaj nowy typ',
+    titlePL: 'Tytuł pl',
+    titleEN: 'Tytuł en',
+    type: 'Typ',
+    createdAt: 'Utworzony',
+    modifiedAt: 'Zmodyfikowany',
+    author: 'Autor',
+  },
+};
