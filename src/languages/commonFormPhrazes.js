ddd535bd47f559739440b00da91d602c90ad67df
marcin <marcinpaczkowski@icloud.com>
2021-04-09 17:01:09 +0200

    Create new translation file

---
+++ src/languages/commonFormPhrazes.js
@@ -0,0 +1,21 @@
+export const commonFormPhrazes = {
+  en: {
+    fieldRequired: 'Field is required',
+    choose: 'choose',
+    save: 'Save',
+    exit: 'Exit',
+    yes: 'Yes',
+    no: 'No',
+    published: 'Published?',
+  },
+
+  pl: {
+    fieldRequired: 'Pole wymagane',
+    choose: 'wybierz',
+    save: 'Zapisz',
+    exit: 'Zamknij',
+    yes: 'Tak',
+    no: 'Nie',
+    published: 'Opublikowany?',
+  },
+};
