cb11ae1a6763eff8f86f84ddd0846030505d313e
marcin <marcinpaczkowski@icloud.com>
2021-04-09 16:55:31 +0200

    Add label

--- src/components/molecules/InlineSwitcher/InlineSwitcher.js
+++ src/components/molecules/InlineSwitcher/InlineSwitcher.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Switcher from 'components/atoms/Switcher/Switcher';
 import styled from 'styled-components';
+import FormikLabel from '../FormikLabel/FormikLabel';
 
-const StyledLangWrapper = styled.div`
+const StyledSwitchWrapper = styled.div`
   display: flex;
   align-items: center;
 `;
 
+const StyledWrapper = styled.div`
+  display: block;
+`;
+
 const InlineSwitcher = ({
+  switchName,
   isChecked,
   switchColor,
   notCheckedColor,
@@ -17,38 +23,47 @@ const InlineSwitcher = ({
   labelAfter,
   labelBeforeStyle,
   labelAfterStyle,
+  label,
 }) => (
-  <StyledLangWrapper>
-    <span style={labelBeforeStyle}>{labelBefore}</span>
-    <Switcher
-      isChecked={isChecked}
-      switchColor={switchColor}
-      notCheckedColor={notCheckedColor}
-      type="checkbox"
-      change={change}
-    />
-    <span style={labelAfterStyle}>{labelAfter}</span>
-  </StyledLangWrapper>
+  <StyledWrapper>
+    {label && <FormikLabel label={label} labelFor={switchName} />}
+    <StyledSwitchWrapper>
+      <span style={labelBeforeStyle}>{labelBefore}</span>
+      <Switcher
+        name={switchName}
+        isChecked={isChecked}
+        switchColor={switchColor}
+        notCheckedColor={notCheckedColor}
+        type="checkbox"
+        change={change}
+      />
+      <span style={labelAfterStyle}>{labelAfter}</span>
+    </StyledSwitchWrapper>
+  </StyledWrapper>
 );
 
 InlineSwitcher.propTypes = {
-  isChecked: propTypes.bool,
-  switchColor: propTypes.string.isRequired,
-  notCheckedColor: propTypes.string,
-  change: propTypes.func.isRequired,
-  labelBefore: propTypes.string,
-  labelAfter: propTypes.string,
-  labelBeforeStyle: propTypes.instanceOf(Object),
-  labelAfterStyle: propTypes.instanceOf(Object),
+  switchName: PropTypes.string,
+  isChecked: PropTypes.bool,
+  switchColor: PropTypes.string.isRequired,
+  notCheckedColor: PropTypes.string,
+  change: PropTypes.func.isRequired,
+  labelBefore: PropTypes.string,
+  labelAfter: PropTypes.string,
+  labelBeforeStyle: PropTypes.instanceOf(Object),
+  labelAfterStyle: PropTypes.instanceOf(Object),
+  label: PropTypes.string,
 };
 
 InlineSwitcher.defaultProps = {
+  switchName: undefined,
   isChecked: false,
   notCheckedColor: 'dark',
   labelBefore: null,
   labelAfter: null,
   labelBeforeStyle: null,
   labelAfterStyle: null,
+  label: '',
 };
 
 export default InlineSwitcher;
