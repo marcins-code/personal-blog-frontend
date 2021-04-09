7b1f4cdc03d67cac4ba5e4f05031ebf3d656994e
marcin <marcinpaczkowski@icloud.com>
2021-04-09 11:53:44 +0200

    Remove unused files

--- src/themes/commonElements/backgroundImages.js
+++ src/themes/commonElements/backgroundImages.js
@@ -1,25 +1,13 @@
 import blackOrchid from 'assets/images/backgrounds/black-orchid.png';
-import hexellence from 'assets/images/backgrounds/hexellence.png';
-import hexellenceMore from 'assets/images/backgrounds/hexellence-more.png';
 import textile from 'assets/images/backgrounds/clean-textile.png';
-import smoothWallLigth from 'assets/images/backgrounds/smooth-wall-light.png';
-import nami from 'assets/images/backgrounds/nami.png';
 import debutLight from 'assets/images/backgrounds/debut-light.png';
-import blackLinen from 'assets/images/backgrounds/black-linen.png';
-import felt from 'assets/images/backgrounds/felt.png';
 import ironGrip from 'assets/images/backgrounds/iron-grip.png';
 import blackPaper from 'assets/images/backgrounds/black-paper.png';
 
 export const backgroundImages = {
   textile,
-  hexellence,
-  hexellenceMore,
   blackOrchid,
-  smoothWallLigth,
-  nami,
   debutLight,
-  blackLinen,
-  felt,
   ironGrip,
   blackPaper,
 };
