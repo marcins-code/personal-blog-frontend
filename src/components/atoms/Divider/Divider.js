1a9e62c8274c974b8233a7a7da71330a80989b47
marcin <marcinpaczkowski@icloud.com>
2021-04-11 07:30:50 +0200

    New component

---
+++ src/components/atoms/Divider/Divider.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { lighten, darken, transparentize } from 'polished';
+
+const Divider = styled.hr`
+  width: 100%;
+  margin-bottom: 30px;
+  border-width: 2px;
+  border-color: ${({ theme }) => lighten(0.05, theme.appBackgroundColor)};
+  box-shadow: 0 0px 1px 0px
+    ${({ theme }) => transparentize(0.2, darken(0.1, theme.appBackgroundColor))};
+  border-radius: 10px;
+`;
+
+export default Divider;
