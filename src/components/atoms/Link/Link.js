9b43ec7513f7fb10982be612a335e6c2fba567ae
marcin <marcinpaczkowski@icloud.com>
2021-04-11 07:31:26 +0200

    Add props

--- src/components/atoms/Link/Link.js
+++ src/components/atoms/Link/Link.js
@@ -1,8 +1,33 @@
-import styled from 'styled-components';
+/* eslint-disable react/prop-types */
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+import styled, { css } from 'styled-components';
+import { getContrast } from 'polished';
 
 const Link = styled.a`
-  color: ${({ theme }) => theme.color};
+  color: ${({ theme }) => theme.secondary};
   cursor: pointer;
+  ${(props) => props.btncolor
+    && css`
+      background: ${({ theme, btncolor }) => theme[btncolor]};
+      color: ${({ theme, btncolor }) => (getContrast(theme[btncolor], theme.grey100) > 4.1 ? theme.grey100 : theme.wax)};
+      padding: 10px;
+      border-radius: 20%;
+    `}
 `;
+
+// const Link = ({ children, href, to }) => (
+//   <StyledLink href={href}>{children}</StyledLink>
+
+// );
+
+// Link.propTypes = {
+//   children: PropTypes.node,
+// };
+
+// Link.defaultProps = {
+//   children: null,
+// };
 
 export default Link;
