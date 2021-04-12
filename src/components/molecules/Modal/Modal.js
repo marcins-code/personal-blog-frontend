d598eb88fbaf7919361331cc28fd791aefc1468d
marcin <marcinpaczkowski@icloud.com>
2021-04-12 10:16:48 +0200

    Add custom options

--- src/components/molecules/Modal/Modal.js
+++ src/components/molecules/Modal/Modal.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { CSSTransition } from 'react-transition-group';
+import { lighten } from 'polished';
 import './animations.css';
+import { PageContext } from 'context';
 import Backdrop from 'components/atoms/Backdrop/Backdrop';
 import Divider from 'components/atoms/Divider/Divider';
-import { lighten } from 'polished';
+import { commonFormPhrazes } from 'languages/commonFormPhrazes';
+import Button from 'components/atoms/Button/Button';
 
 const StyledModalBody = styled.div`
-  overflow-y: auto;
-  z-index: 3100;
   position: fixed;
-  margin: 0 auto;
-  width: 50%;
-  left: 25%;
+  width: 40%;
+  height: 40%;
+  left: 30%;
   top: 12vh;
   background-image: url(${({ theme }) => theme.appBackgrounImage});
   background-color: ${({ theme }) => theme.appBackgroundColor};
@@ -22,48 +23,100 @@ const StyledModalBody = styled.div`
   box-shadow: 0 0 90px -10px ${({ theme }) => lighten(0.1, theme.appBoxShadowColor)} inset,
     0 0 60px -10px rgba(0, 0, 0, 0.5);
   color: ${({ theme }) => theme.color};
-  height: 500px;
   border-radius: 15px;
-  align-self: center;
-  /* padding: 20px 0; */
-  scrollbar-color: #252525 #282a35;
-  scrollbar-width: auto;
-  &::-webkit-scrollbar {
-    width: 30px; /* width of the entire scrollbar */
-  }
-
-  &::-webkit-scrollbar-track {
-    background: #252525;
-    border-radius: 30px; /* color of the tracking area */
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: #282a35; /* color of the scroll thumb */
-    border-radius: 200px; /* roundness of the scroll thumb */
-  }
+  z-index: 3100;
+  ${(props) => props.modalSmall
+    && css`
+      width: 25%;
+      height: 25%;
+      left: 37.5%;
+    `}
+  ${(props) => props.modalBig
+    && css`
+      width: 65% !important;
+      height: 65%;
+      left: 17.5%;
+    `}
 `;
 
 const StyledModalHeader = styled.div`
   padding: 10px 20px;
-  /* position: fixed; */
   height: 40px;
-  /* width: 100%; */
+  margin-bottom: 10px;
 `;
 
 const StyledModalContent = styled.div`
-  padding: 70px 30px 40px 30px;
+  position: relative;
+  overflow-y: auto;
+  overflow-x: hidden;
+  height: 63%;
+  margin: 30px 0 5px 0;
+  padding: 10px 30px;
+  ${(props) => props.modalBig
+    && css`
+      height: 76%;
+    `}
+
+  ${(props) => props.modalSmall
+    && css`
+      height: 40%;
+    `}
+      scrollbar-color: black green;
+  scrollbar-width: thin;
+
+  &::-webkit-scrollbar {
+    width: 9px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: ${({ theme }) => theme.appBackgroundColor};
+    border-radius: 20px;
+    overflow: hidden;
+    margin: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => lighten(0.1, theme.appBackgroundColor)};
+    border-radius: 200px;
+  }
 `;
 
-const StyledModalButtons = styled.div``;
+// const StyledModalContent = styled.div``;
+
+const StyledModalButtons = styled.div`
+  position: absolute;
+  bottom: 0;
+  padding: 15px 10px;
+  width: 100%;
+  > button {
+    margin-right: 20px;
+  }
+`;
 
 const Modal = ({
-  modalHeader, modalContent, modalShow, resetFunc,
+  modalHeader,
+  modalContent,
+  modalShow,
+  resetFunc,
+  modalSmall,
+  modalBig,
+  buttonClose,
+  buttonConfirmDanger,
+  buttonConfirmDangerLabel,
+  buttonConfirmDangerLabelIcon,
+  confirmDangerFunc,
+  buttonConfirmSucess,
+  buttonConfirmSuccessLabel,
+  buttonConfirmSuccessLabelIcon,
+  confirmSuccessFunc,
+  noBackdropClose,
 }) => {
+  const { lang } = useContext(PageContext);
+
   const content = (
     <>
-      {modalShow && <Backdrop onClick={() => resetFunc(null)} />}
+      {modalShow && <Backdrop onClick={() => !noBackdropClose && resetFunc(null)} />}
       <CSSTransition in={modalShow} mountOnEnter unmountOnExit timeout={1000} classNames="modal">
-        <StyledModalBody>
+        <StyledModalBody modalSmall={modalSmall} modalBig={modalBig}>
           {modalHeader && (
             <>
               <StyledModalHeader>
@@ -72,10 +125,52 @@ const Modal = ({
               </StyledModalHeader>
             </>
           )}
-          <StyledModalContent>
+          <StyledModalContent modalBig={modalBig} modalSmall={modalSmall}>
             {modalContent}
-            <StyledModalButtons />
           </StyledModalContent>
+          <StyledModalButtons>
+            <Divider style={{ marginBottom: '10px' }} />
+            {buttonClose && (
+              <Button
+                btnColor="primary"
+                labelIcon={['far', 'times-circle']}
+                label={commonFormPhrazes[lang].close}
+                btnClick={() => resetFunc(null)}
+              />
+            )}
+            {buttonConfirmDanger && (
+              <>
+                <Button
+                  btnColor="danger"
+                  labelIcon={buttonConfirmDangerLabelIcon}
+                  label={buttonConfirmDangerLabel || commonFormPhrazes[lang].confirm}
+                  btnClick={confirmDangerFunc}
+                />
+                <Button
+                  btnColor="dark"
+                  labelIcon={['far', 'times-circle']}
+                  label={commonFormPhrazes[lang].cancel}
+                  btnClick={() => resetFunc(null)}
+                />
+              </>
+            )}
+            {buttonConfirmSucess && (
+              <>
+                <Button
+                  btnColor="success"
+                  labelIcon={buttonConfirmSuccessLabelIcon || ['fas', 'check']}
+                  label={buttonConfirmSuccessLabel || commonFormPhrazes[lang].confirm}
+                  btnClick={confirmSuccessFunc}
+                />
+                <Button
+                  btnColor="dark"
+                  labelIcon={['far', 'times-circle']}
+                  label={commonFormPhrazes[lang].cancel}
+                  btnClick={() => resetFunc(null)}
+                />
+              </>
+            )}
+          </StyledModalButtons>
         </StyledModalBody>
       </CSSTransition>
     </>
@@ -88,11 +183,27 @@ Modal.propTypes = {
   modalContent: PropTypes.instanceOf(Object).isRequired,
   modalShow: PropTypes.bool,
   resetFunc: PropTypes.func.isRequired,
+  modalSmall: PropTypes.bool,
+  modalBig: PropTypes.bool,
+  buttonClose: PropTypes.bool,
+  buttonConfirmDanger: PropTypes.bool,
+  confirmDangerFunc: PropTypes.func,
+  buttonConfirmDangerLabel: PropTypes.string,
+  buttonConfirmDangerLabelIcon: PropTypes.instanceOf(Array),
 };
 
 Modal.defaultProps = {
   modalHeader: null,
   modalShow: false,
+  modalSmall: false,
+  modalBig: false,
+  buttonClose: false,
+  buttonConfirmDanger: false,
+  confirmDangerFunc: undefined,
+  buttonConfirmDangerLabel: null,
+  buttonConfirmDangerLabelIcon: [],
 };
 
 export default Modal;
+
+// TODO scroll styles
