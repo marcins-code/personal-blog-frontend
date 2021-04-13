22a5f461ebae93bc63cc970531e95bd72723cd67
marcin <marcinpaczkowski@icloud.com>
2021-04-13 09:10:17 +0200

    Add new translation file for glossary

---
+++ src/languages/glossaryPagePhrazes.js
@@ -0,0 +1,13 @@
+export const glossaryPagePhrazes = {
+  en: {
+    title: 'Glossary',
+    formTitle: 'Create and edit glossary entries',
+    newEntry: 'Add new entry',
+  },
+
+  pl: {
+    title: 'Słowniczek',
+    newEntry: 'Dodaj wpis',
+    formTitle: 'Tworzenie i edycja wpisów słowniczka',
+  },
+};
