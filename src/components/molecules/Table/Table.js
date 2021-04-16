b090b26732783af9c97dce90d17e148a123a9f5a
marcin <marcinpaczkowski@icloud.com>
2021-04-16 18:21:02 +0200

    Cahnge folder

--- src/components/molecules/Table/Table.js
+++ src/components/molecules/Table/Table.js
@@ -1,80 +1,108 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import { darken } from 'polished';
+import {
+  darken, transparentize, getContrast, lighten,
+} from 'polished';
 
 const StyledTableWrapper = styled.div`
   position: relative;
   width: 95%;
-  background-color: rgba(0, 0, 0, 0.05);
   border-radius: 20px;
   border: solid 3px rgba(0, 0, 0, 0.25);
   box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.15);
-  overflow: scroll;
-  ${(props) => props.tblCenter
-    && css`
-      margin: auto;
-    `}
+  overflow: hidden;
+
+  background-color: ${({ theme }) => (theme.themeName !== 'light'
+    ? transparentize(0.85, theme.black)
+    : transparentize(0.85, theme.grey100))};
 `;
 
 const StyledTable = styled.table`
   border-collapse: collapse;
   width: 100%;
   display: table;
-  /* overflow: scroll; */
-  ${(props) => props.tblHover
+  ${(props) => props.tblColor
     && css`
       > tbody > tr {
         &:hover {
-          background-color: rgba(0, 107, 169, 0.15);
-          cursor: pointer;
+          background-color: ${({ theme }) => (props.tblColor
+    ? transparentize(0.75, theme[props.tblColor])
+    : transparentize(0.75, theme.black))};
         }
       }
     `}
 `;
 
 const StyledTableHeader = styled.thead`
-  background: linear-gradient(
-    to right,
-    ${({ theme }) => theme.blue} 0%,
-    ${({ theme }) => darken(0.1, theme.blue)} 50%,
-    ${({ theme }) => theme.blue} 100%
-  );
+  ${(props) => props.tblColor
+    && css`
+      box-shadow: 0 2px 16px 1px rgba(0, 0, 0, 0.4);
+      background: linear-gradient(
+        90deg,
+        ${({ theme }) => darken(0.1, theme[props.tblColor])} 20%,
+        ${({ theme }) => darken(0.05, theme[props.tblColor])} 50%,
+        ${({ theme }) => darken(0.03, theme[props.tblColor])} 70%,
+        ${({ theme }) => darken(0.1, theme[props.tblColor])} 80%
+      );
+      & > tr {
+        border-bottom: 5px inset ${({ theme }) => lighten(0.06, theme[props.tblColor])};
+      }
+      & > tr > th {
+        color: ${({ theme }) => (getContrast(theme[props.tblColor], theme.grey100) > 4.1 ? theme.grey100 : theme.wax)};
+        padding-top: 5px;
+      }
+    `}
   background-attachment: fixed;
+
   > tr {
     height: 55px;
-
-    /* border-bottom: solid 3px black; */
   }
 `;
 
 const StyledTableBody = styled.tbody`
-  > tr:nth-child(odd) {
-    background: rgba(0, 0, 0, 0.2);
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    border-bottom: solid 1px rgba(0, 0, 0, 0.8);
-    border-top: solid 1px rgba(0, 0, 0, 0.8);
-  }
   > tr > td {
     padding: 10px 20px;
     text-align: center;
-    /* border-spacing: 0; */
   }
+  ${(props) => props.tblStripped
+    && css`
+      > tr:nth-child(odd) {
+        background: ${({ theme }) => (theme.themeName !== 'light'
+    ? transparentize(0.8, theme.black)
+    : transparentize(0.75, theme.grey100))};
+        border-top: 1px solid
+          ${({ theme }) => (theme.themeName !== 'light'
+    ? transparentize(0.05, theme.black)
+    : transparentize(0.05, theme.grey100))};
+        border-bottom: 1px solid
+          ${({ theme }) => (theme.themeName !== 'light'
+    ? transparentize(0.05, theme.black)
+    : transparentize(0.05, theme.grey100))};
+      }
+
+      > tr:last-child {
+        border-bottom: none !important;
+      }
+      > tr:first-child {
+        border-top: none;
+      }
+    `}
 `;
 
 const Table = ({
-  headerItems, children, tblCenter, tblHover, loading,
+  headerItems, children, tblHover, tblStripped, tblColor,
 }) => (
-  <StyledTableWrapper tblCenter>
-    <StyledTable tblHover>
-      <StyledTableHeader>
+  <StyledTableWrapper className="fadeIn" t>
+    <StyledTable tblHover={tblHover} tblColor={tblColor}>
+      <StyledTableHeader tblColor={tblColor}>
         <tr>
           {headerItems.map((item) => (
             <th key={item.id}>{item.title}</th>
           ))}
         </tr>
       </StyledTableHeader>
-      <StyledTableBody>{children}</StyledTableBody>
+      <StyledTableBody tblStripped={tblStripped}>{children}</StyledTableBody>
     </StyledTable>
   </StyledTableWrapper>
 );
@@ -82,17 +110,17 @@ const Table = ({
 Table.propTypes = {
   headerItems: PropTypes.instanceOf(Array),
   children: PropTypes.node,
-  tblCenter: PropTypes.bool,
   tblHover: PropTypes.bool,
-  loading: PropTypes.bool,
+  tblStripped: PropTypes.bool,
+  tblColor: PropTypes.string,
 };
 
 Table.defaultProps = {
   headerItems: [],
   children: null,
-  tblCenter: false,
   tblHover: false,
-  loading: false,
+  tblStripped: false,
+  tblColor: null,
 };
 
 export default Table;
