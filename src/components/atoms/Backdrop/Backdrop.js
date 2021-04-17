5760cb43ba70ec8cdceb4855e219517cf5b73dbc
marcin <marcinpaczkowski@icloud.com>
2021-04-17 13:28:40 +0200

    Add fading animation

--- src/components/atoms/Backdrop/Backdrop.js
+++ src/components/atoms/Backdrop/Backdrop.js
@@ -1,24 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { CSSTransition } from 'react-transition-group';
+import { transparentize } from 'polished';
+import './animations.css';
 
-const Backdrop = styled.div`
+const StyledBackdrop = styled.div`
   position: fixed;
   height: 100%;
   width: 100%;
-  background-color: rgba(30, 30, 30, 0.8);
+  background-color: ${({ theme }) => transparentize(0.15, theme.grey900)};
   z-index: 301;
   top: 0;
   left: 0;
-  opacity: 1;
-  animation: fadein 300ms;
-
-  @keyframes fadein {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
+  /* opacity: 1; */
 `;
 
+const Backdrop = ({ onClick, bckdShow }) => (
+  <CSSTransition in={bckdShow} mountOnEnter unmountOnExit timeout={900} classNames="backdrop">
+    <StyledBackdrop onClick={onClick} />
+  </CSSTransition>
+);
+
+Backdrop.propTypes = {
+  onClick: PropTypes.func,
+  bckdShow: PropTypes.bool.isRequired,
+};
+
+Backdrop.defaultProps = {
+  onClick: undefined,
+};
 export default Backdrop;
