525f3a3e5e26aca13b4e16b1474dcf76aed80054
marcin <marcinpaczkowski@icloud.com>
2021-04-19 05:52:43 +0200

    Refactor comments

--- src/components/atoms/Wrappers/InputWrapper.js
+++ src/components/atoms/Wrappers/InputWrapper.js
@@ -2,12 +2,7 @@ import styled from 'styled-components';
 
 const InputWrapper = styled.div`
   padding: 10px;
-  margin-bottom: 25px;
   position: relative;
-  display: flex;
-  flex-direction: column;
 `;
 
 export default InputWrapper;
-
-// TODO improve positioning with props
