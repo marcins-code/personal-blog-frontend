a5fe657207ee2842e6b9e39e25c5bd784a38b5e7
marcin <marcinpaczkowski@icloud.com>
2021-04-19 07:57:26 +0200

    Change text shadow

--- src/themes/elements/appThemes.js
+++ src/themes/elements/appThemes.js
@@ -26,8 +26,8 @@ export const lightTheme = {
   appBoxShadowColor: 'rgba(20,20,20,.9)',
   menuBackgrounImage: backgroundImages.blackPaper,
   menuTextShadow:
-    '0px 0px 0 rgb(235, 235, 235), 1px 1px 0 rgb(124,124, 124), 1px 1px 0 rgb(-86, -86, -86)',
-  color: commonColors.grey900,
+    '0px 0px 0 rgb(235, 235, 235), 1px 1px 0 rgb(174,174, 174), 1px 1px 0 rgb(-86, -86, -86)',
+  color: commonColors.grey800,
   primary: commonColors.cyan,
   secondary: complement(commonColors.cyan),
   success: commonColors.green,
