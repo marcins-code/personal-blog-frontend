f8390aba6e57dae3ec0e5a34adaf34752b64ee4f
marcin <marcinpaczkowski@icloud.com>
2021-05-05 11:08:37 +0200

    Add delete modal

--- src/views/admin/AdminArticles.js
+++ src/views/admin/AdminArticles.js
@@ -5,9 +5,8 @@ import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
 import parse from 'html-react-parser';
-import { toast } from 'react-toastify';
 import { device } from 'themes/commonElements/mediaBreakpoints';
-import { PageContext, AuthContext } from 'context';
+import { PageContext } from 'context';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { commonPhrazes } from 'languages/commonPhrazes';
 import { articlePagePhrazes } from 'languages/articlePagePhrazes';
@@ -17,8 +16,8 @@ import MainTemplate from 'templates/MainTemplate';
 import Button from 'components/atoms/Button/Button';
 import Spinner from 'components/atoms/Spinner/Spinner';
 import Table from 'components/molecules/Table/Table';
-import Modal from 'components/molecules/Modal/Modal';
 import ErrorBox from 'components/molecules/ErrorBox/ErrorBox';
+import DeleteModal from 'components/organism/Modals/DeleteModal';
 
 const StyledSubheader = styled.div`
   display: flex;
@@ -47,29 +46,33 @@ const StyledOnlyBigScreenCell = styled.td`
 const AdminArticles = () => {
   const { push } = useHistory();
   const { lang } = useContext(PageContext);
-  const auth = useContext(AuthContext);
 
   const TableHeader = [
     { id: 1, title: articlePagePhrazes[lang].articleTitle },
     { id: 2, title: articlePagePhrazes[lang].type },
-    { id: 3, title: articlePagePhrazes[lang].categorySerie },
-    { id: 4, title: commonPhrazes[lang].published },
-    { id: 5, title: articlePagePhrazes[lang].author },
-    { id: 6, title: commonPhrazes[lang].createdAt },
+    { id: 3, title: articlePagePhrazes[lang].seriePart },
+    { id: 4, title: articlePagePhrazes[lang].categorySerie },
+    { id: 5, title: commonPhrazes[lang].published },
+    { id: 6, title: articlePagePhrazes[lang].author },
+    { id: 7, title: commonPhrazes[lang].createdAt },
     { id: 8, title: '' },
   ];
 
   const [fetchedData, setFetchedData] = useState([]);
   useEffect(() => {
-    axios.get('/article').then((response) => {
-      setFetchedData(response.data);
-      // console.log(response);
-    });
+    axios
+      .get('/article')
+      .then((response) => {
+        setFetchedData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   const [clickedRecordID, setClickedRecordID] = useState('');
   const [clickedRecordName, setClickedRecordName] = useState();
-  // const [modalHeader, setModalHeader] = useState(false);
   const [modalContent, setModalContent] = useState(false);
   const getRecordID = useCallback(
     (e) => {
@@ -95,6 +98,14 @@ const AdminArticles = () => {
     },
     [showDeleteModal],
   );
+  const resetDeletewModalHandler = useCallback(
+    (e) => {
+      setClickedRecordID('');
+      setClickedRecordName('');
+      setShowDeleteModal(false);
+    },
+    [showDeleteModal],
+  );
 
   useEffect(() => {
     setModalContent(
@@ -102,61 +113,32 @@ const AdminArticles = () => {
     );
   }, [showDeleteModal, lang]);
 
-  const deleteConfiramtionHandler = useCallback(
-    (e) => {
-      setConfirmDelete(true);
-      setShowDeleteModal(false);
-    },
-    [confirmDelete],
-  );
+  console.log(confirmDelete);
 
   useEffect(() => {
     if (confirmDelete) {
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${auth.token}`,
-      };
-      confirmDelete
-        && axios
-          .delete(`article/${clickedRecordID}`, { headers })
-          .then((response) => {
-            if (response.status === 200) {
-              setFetchedData(
-                fetchedData && fetchedData.filter((item) => item._id !== clickedRecordID),
-              );
-              setConfirmDelete(false);
-              toast.success(commonPhrazes[lang].deletionSuccess, { autoClose: 3500 });
-              setConfirmDelete(false);
-              setClickedRecordID();
-              setClickedRecordName();
-              setModalContent();
-            }
-          })
-          .catch((error) => {
-            if (error.response) {
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            }
-          });
+      setFetchedData(fetchedData && fetchedData.filter((item) => item._id !== confirmDelete));
+      setModalContent('');
+      setClickedRecordID('');
+      setClickedRecordName('');
+      setConfirmDelete(false);
+      // setShowDeleteModal(false);
     }
-  }, [confirmDelete, lang]);
+  }, [confirmDelete]);
+
+  console.log(clickedRecordID);
 
   return (
     <MainTemplate>
-      <Modal
-        mdlShow={showDeleteModal}
-        buttonConfirmDanger
-        mdlSmall
-        noBackdropClose
-        resetFunc={() => setShowDeleteModal(false)}
-        mdlHeader={commonPhrazes[lang].confirm}
-        mdlHeaderIcon={['fas', 'exclamation']}
-        buttonConfirmDangerLabelIcon={['far', 'trash-alt']}
-        confirmDangerFunc={deleteConfiramtionHandler}
-      >
-        {modalContent}
-      </Modal>
+      <DeleteModal
+        showDeleteModal
+        dltModalTrigger={showDeleteModal}
+        dltMdlResetFunc={resetDeletewModalHandler}
+        dltMdlContent={modalContent}
+        dltMdlIdtoDelete={clickedRecordID}
+        dltModalDeleteApi="/article"
+        dltMdlConfirmDelete={setConfirmDelete}
+      />
       <AdminPageWrapper>
         <PageHeader title={articlePagePhrazes[lang].title} icon={['far', 'file-alt']} />
         <StyledSubheader>
@@ -180,11 +162,12 @@ const AdminArticles = () => {
               <tr key={item._id} id={item._id} data-name={item.title[lang]}>
                 <td>{item.title[lang]}</td>
                 <td>
-                  {item.serie
-                    ? `${commonPhrazes[lang].serie}  - ${item.seriePart}`
+                  {item.articleType.type === 'serie'
+                    ? `${commonPhrazes[lang].serie}`
                     : commonPhrazes[lang].category}
                 </td>
-                <td>{item.category ? item.category : item.serie}</td>
+                <td>{item.articleType.type === 'serie' && item.seriePart}</td>
+                <td>{item.articleType.name}</td>
                 <td>
                   <FontAwesomeIcon
                     icon={item.isEnabled ? ['far', 'check-circle'] : ['far', 'times-circle']}
@@ -218,5 +201,3 @@ const AdminArticles = () => {
   );
 };
 export default AdminArticles;
-
-// Add caych to initial axios all admin files
