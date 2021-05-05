cf7d85ef611d23d11337cbef4bfa08592995d9b9
marcin <marcinpaczkowski@icloud.com>
2021-05-05 11:09:17 +0200

    Refactor hook code

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -4,20 +4,15 @@ import { useState, useCallback } from 'react';
 export const useAxios = (withAuth) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [statusCode, setStatusCode] = useState(null);
 
   const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
-    console.log(requestConfig.body);
+    console.log(requestConfig.method);
 
     const ourRequest = axios.CancelToken.source();
-    // axios({
-    //   method: requestConfig.method,
-    //   url: requestConfig.url,
-    //   headers: requestConfig.headers,
-    //   data: body,
-    //   cancelToken: ourRequest.token,
-    // })
+
     axios({
       method: requestConfig.method ? requestConfig.method : 'GET',
       url: requestConfig.url,
@@ -25,9 +20,9 @@ export const useAxios = (withAuth) => {
       data: requestConfig.body ? requestConfig.body : null,
       cancelToken: ourRequest.token,
     })
-      .then((response) => response.data)
-      .then((data) => {
-        applyData(data);
+      .then((response) => {
+        applyData(response.data);
+        setStatusCode(response.status);
       })
       .catch((err) => {
         if (err.response) {
@@ -42,5 +37,10 @@ export const useAxios = (withAuth) => {
     };
   }, []);
 
-  return { isLoading, error, sendRequest };
+  return {
+    isLoading,
+    error,
+    statusCode,
+    sendRequest,
+  };
 };
